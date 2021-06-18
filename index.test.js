536ec68384ced34c3ec38ed1daae5531f070004e
MomoCow <momocow.me@gmail.com>
2021-06-19 02:06:16 +0800

    :white_check_mark: Update testcase format.

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
-const exp = require('constants')
 const fs = require('fs')
 const path = require('path')
+
+const { buildTestcaseTable } = require('./scripts/testcase')
 
 const problemsDir = path.join(__dirname, 'problems')
 
@@ -8,19 +9,32 @@ for (const problem of fs.readdirSync(problemsDir)) {
     const problemDir = path.resolve(problemsDir, problem)
     if (fs.statSync(problemDir).isDirectory()) {
         const testcasesFile = path.resolve(problemDir, 'testcases')
-        const envFile = path.resolve(problemDir, 'env')
+        const envFile = path.resolve(problemDir, 'env.js')
+        const setupFile = path.resolve(problemDir, 'setup.js')
 
-        const fn = require(problemDir)
         if (fs.existsSync(envFile)) {
             Object.assign(global, require(envFile))
         }
 
+        let inputDeserializer = JSON.parse
+        let outputSerializer = JSON.stringify
+        if (fs.existsSync(setupFile)) {
+            const { serializer, deserializer } = require(setupFile)
+            inputDeserializer = deserializer || inputDeserializer
+            outputSerializer = serializer || outputSerializer
+        }
+
+        const fn = require(problemDir)
+
         describe(problem, () => {
-            test.each(require(testcasesFile))('Case #%#', (...args) => {
-                const inputs = args.slice(0, -1)
-                const expected = args.slice(-1)
-                expect(fn(...inputs)).toEqual(expected[0])
-            })
+            test.each(buildTestcaseTable(testcasesFile, inputDeserializer))(
+                'Case #%#',
+                (...args) => {
+                    const inputs = args.slice(0, -1)
+                    const expected = args.slice(-1)
+                    expect(outputSerializer(fn(...inputs))).toEqual(expected[0])
+                }
+            )
         })
     }
 }
---
+++ scripts/testcase.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+
+module.exports.buildTestcaseTable = function (testcasesFile, inputDeserializer = JSON.parse) {
+    let prevLines = 0
+    let blocks = fs.readFileSync(testcasesFile, 'utf-8').split(/(?:\r?\n){2}/)
+    const [ inputs, expects ] = blocks.reduce((cases, block, blockno) => {
+        const lines = block.split(/\r?\n/)
+
+        // 1st block is "Your input"
+        // last block is "Expected"
+        if (blockno === 0 || blockno === blocks.length - 1) {
+            // 1st line in each block is the block title
+            let blockLines = lines.slice(1).map(l => l.trim()).filter(l => !!l)
+            if (blockno === 0) {
+                blockLines = blockLines.map(l => inputDeserializer(l))
+            }
+            cases.push(blockLines)
+        }
+        prevLines += lines.length + 1 // blank line
+        return cases
+    }, [])
+    
+    if (inputs.length % expects.length !== 0) {
+        throw new Error('Mismatch between number of testcases and number of expected values')
+    }
+
+    const inputsPerCase = inputs.length / expects.length
+    return inputs
+        .reduce((table, input, i) => {
+            const caseno = Math.floor(i / inputsPerCase)
+            while (table.length < caseno + 1) {
+                table.push([])
+            }
+            table[caseno].push(input)
+            return table
+        }, [])
+        .reduce((table, testcase, i) => {
+            table.push(testcase.concat([expects[i]]))
+            return table
+        }, [])
+}
