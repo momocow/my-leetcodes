3dcaa8e567a00e206c2a689e6d90b548f16e8509
MomoCow <momocow.me@gmail.com>
2021-07-03 12:06:40 +0800

    :sparkles: Add P658.

---
+++ problems/658. Find K Closest Elements/index.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} arr
+ * @param {number} target
+ * @param {number} start
+ * @param {number} end
+ * @return {number}
+ */
+function binarySearch (arr, target, start = 0, end = arr.length) {
+  if (arr[end] <= target) return end
+  if (arr[start] >= target) return start
+  while (end - start > 1) {
+    const half = Math.round((end + start) / 2)
+    if (arr[half] > target) {
+      end = half
+    } else if (arr[half] < target) {
+      start = half
+    } else {
+      return half
+    }
+  }
+  return (target - arr[start]) > (arr[end] - target) ? end : start
+}
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+  const idx = binarySearch(arr, x)
+  const ret = [arr[idx]]
+  let left = idx - 1
+  let right = idx + 1
+  while (--k) {
+    if (right >= arr.length || x - arr[left] <= arr[right] - x) {
+      ret.unshift(arr[left])
+      left--
+    } else {
+      ret.push(arr[right])
+      right++
+    }
+  }
+
+  return ret
+}
+
+module.exports = findClosestElements
