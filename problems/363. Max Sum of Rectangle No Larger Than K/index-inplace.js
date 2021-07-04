e9375b5c84efc33393b23872a48d86cde1cdfdf9
MomoCow <momocow.me@gmail.com>
2021-07-04 23:39:39 +0800

    :zap: Improve P363.

--- problems/363. Max Sum of Rectangle No Larger Than K/index-inplace.js
+++ problems/363. Max Sum of Rectangle No Larger Than K/index-inplace.js
@@ -7,27 +7,28 @@ var maxSumSubmatrix = function (matrix, k) {
   /**
    * @type {number[][]}
    */
-  const sumMap = []
+  // const sumMap = []
 
   let maxSum = -Infinity
+  let doneRow = -1
+  let doneCol = -1
   for (let row1 = 0; row1 < matrix.length; row1++) {
     for (let col1 = 0; col1 < matrix[row1].length; col1++) {
       for (let row2 = row1; row2 < matrix.length; row2++) {
         let rowSum = 0
-        if (!Array.isArray(sumMap[row2])) {
-          sumMap[row2] = []
-        }
         for (let col2 = col1; col2 < matrix[row1].length; col2++) {
-          if (typeof sumMap[row2][col2] !== 'number') {
+          if (row2 > doneRow || col2 > doneCol) {
             rowSum += matrix[row2][col2]
-            sumMap[row2][col2] = rowSum + (sumMap[row2 - 1]?.[col2] ?? 0)
+            matrix[row2][col2] = rowSum + (matrix[row2 - 1]?.[col2] ?? 0)
+            doneRow = row2
+            doneCol = col2
           }
 
           // calculate local sub-matrix
-          const rect = sumMap[row2][col2]
-          const rectLeftTopRightTop = sumMap[row1 - 1]?.[col2] ?? 0
-          const rectLeftTopLeftBottom = sumMap[row2][col1 - 1] ?? 0
-          const rectLeftTop = sumMap[row1 - 1]?.[col1 - 1] ?? 0
+          const rect = matrix[row2][col2]
+          const rectLeftTopRightTop = matrix[row1 - 1]?.[col2] ?? 0
+          const rectLeftTopLeftBottom = matrix[row2][col1 - 1] ?? 0
+          const rectLeftTop = matrix[row1 - 1]?.[col1 - 1] ?? 0
           const rectRightBottom = (
             rect -
             rectLeftTopLeftBottom -
