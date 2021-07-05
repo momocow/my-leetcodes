4df80fc7499e838aed03f970e865d23997528ecc
MomoCow <momocow.me@gmail.com>
2021-07-05 19:16:56 +0800

    :zap: Improve P1220 by less variable used.
    Based on the result of analyze.js.

--- problems/1220. Count Vowels Permutation/index.js
+++ problems/1220. Count Vowels Permutation/index.js
@@ -5,24 +5,21 @@ const MOD = 1000000007
  * @return {number}
  */
 var countVowelPermutation = function (n) {
-  let a = 1
-  let e = 1
-  let i = 1
-  let o = 1
-  let u = 1
+  let p1 = 1 // aeiou
+  let p2 = 0 // aaeiu
+  let p3 = 0 // aaee
+  let p4 = 0 // ii
   while (--n) {
-    const aa = a
-    const ee = e
-    const ii = i
-    const oo = o
-    const uu = u
-    a = (ee + ii + uu) % MOD
-    e = (aa + ii) % MOD
-    i = (ee + oo) % MOD
-    o = (ii) % MOD
-    u = (ii + oo) % MOD
+    const pp1 = p1
+    const pp2 = p2
+    const pp3 = p3
+    const pp4 = p4
+    p1 = (pp1 + pp2 + 2 * pp4) % MOD
+    p2 = pp1
+    p3 = (pp2 + pp3) % MOD
+    p4 = (pp3 - pp4 + MOD) % MOD
   }
-  return (a + e + i + o + u) % MOD
+  return (p1 * 5 + p2 * 5 + p3 * 4 + p4 * 2) % MOD
 }
 
 module.exports = countVowelPermutation
