4d611665dec64ca5a805f538c793016380bda075
MomoCow <momocow.me@gmail.com>
2021-07-06 15:47:59 +0800

    :sparkles: Add P1338.

---
+++ problems/1338. Reduce Array Size to The Half/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var minSetSize = function (arr) {
+  const counter = new Map()
+  const thres = arr.length / 2
+  for (const item of arr) {
+    counter.set(item, (counter.get(item) ?? 0) + 1)
+  }
+  const freq = Array.from(counter)
+    .sort(([, v1], [, v2]) => v2 - v1)
+  let ret = 0
+  let total = 0
+  for (const [, v] of freq) {
+    ret++
+    total += v
+    if (total >= thres) break
+  }
+  return ret
+}
+
+module.exports = minSetSize
