bc819722b56fa40264c179a47fb66a493586a569
MomoCow <momocow.me@gmail.com>
2021-07-06 16:05:37 +0800

    :zap: Improve P1338.

--- problems/1338. Reduce Array Size to The Half/index.js
+++ problems/1338. Reduce Array Size to The Half/index.js
@@ -8,11 +8,11 @@ var minSetSize = function (arr) {
   for (const item of arr) {
     counter.set(item, (counter.get(item) ?? 0) + 1)
   }
-  const freq = Array.from(counter)
-    .sort(([, v1], [, v2]) => v2 - v1)
+  const freq = Array.from(counter.values())
+    .sort((v1, v2) => v2 - v1)
   let ret = 0
   let total = 0
-  for (const [, v] of freq) {
+  for (const v of freq) {
     ret++
     total += v
     if (total >= thres) break
