aab52d54e314212b6f034cbbcf99e4d8dcde0ea4
MomoCow <momocow.me@gmail.com>
2021-07-07 18:52:04 +0800

    :zap: Improve P378.

--- problems/378. Kth Smallest Element in a Sorted Matrix/index.js
+++ problems/378. Kth Smallest Element in a Sorted Matrix/index.js
@@ -7,12 +7,12 @@ var kthSmallest = function (matrix, k) {
   const cols = matrix[0].length
   const colIdxs = new Array(matrix.length).fill(0)
   while (true) {
-    let smallestRow = -1
-    for (let row = 0; row < matrix.length; row++) {
-      if (colIdxs[row] < cols && (
-        smallestRow < 0 ||
+    let smallestRow = colIdxs.findIndex(n => n < cols)
+    for (let row = 1; row < matrix.length; row++) {
+      if (
+        colIdxs[row] < cols &&
         matrix[row][colIdxs[row]] < matrix[smallestRow][colIdxs[smallestRow]]
-      )) {
+      ) {
         smallestRow = row
       }
     }
