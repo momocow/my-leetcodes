32ce1a682875a8bfb1fb5186c431cabf44d62dc8
MomoCow <momocow.me@gmail.com>
2021-07-08 17:48:04 +0800

    :zap: Improve P718. Double index. Still TLE.

--- problems/718. Maximum Length of Repeated Subarray/index.js
+++ problems/718. Maximum Length of Repeated Subarray/index.js
@@ -1,27 +1,51 @@
+function buildIndex (arr, start = 0, len = arr.length) {
+  let ss = ''
+  for (let j = 0; j < len; j++) {
+    ss += arr[start + j]
+    if (j < len - 1) {
+      ss += ','
+    }
+  }
+  return ss
+}
+
+function buildAccSum (arr) {
+  let sum = 0
+  return arr.map(n => (sum += n))
+}
+
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
 var findLength = function (nums1, nums2) {
-  let w = Math.min(nums1.length, nums2.length)
+  if (nums1.length > nums2.length) {
+    const tmp = nums1
+    nums1 = nums2
+    nums2 = tmp
+  }
+  let w = nums1.length
+  const as1 = buildAccSum(nums1)
+  const as2 = buildAccSum(nums2)
   do {
+    const idx = new Set()
+    const as = new Set()
     for (let i = 0; i <= nums1.length - w; i++) {
-      for (let j = 0; j <= nums2.length - w; j++) {
-        let same = true
-        for (let k = 0; k < w; k++) {
-          if (nums1[i + k] !== nums2[j + k]) {
-            same = false
-            break
-          }
-        }
-        if (same) {
-          return w
-        }
+      idx.add(buildIndex(nums1, i, w))
+      as.add(as1[i + w - 1] - (as1[i - 1] ?? 0))
+    }
+    for (let i = 0; i <= nums2.length - w; i++) {
+      if (
+        as.has(as2[i + w - 1] - (as2[i - 1] ?? 0)) &&
+        idx.has(buildIndex(nums2, i, w))
+      ) {
+        return w
       }
     }
-  } while (--w)
-  return w
+  } while (w--)
+
+  return 0
 }
 
 module.exports = findLength
