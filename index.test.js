5f34844de6e58bcec71edd05d32d1a9dca84f00c
MomoCow <momocow.me@gmail.com>
2021-07-09 13:54:01 +0800

    :white_check_mark: Add a new structure, ListNode.

--- index.test.js
+++ index.test.js
@@ -3,11 +3,13 @@ const path = require('path')
 
 const { buildTestcaseTable, runTestcase } = require('./lib/testcase')
 const { TreeNode } = require('./lib/tree')
+const { ListNode } = require('./lib/list')
 
 const problemsDir = path.join(__dirname, 'problems')
 
 beforeAll(() => {
   global.TreeNode = TreeNode
+  global.ListNode = ListNode
 })
 
 afterAll(() => {
@@ -18,11 +20,13 @@ for (const problem of fs.readdirSync(problemsDir)) {
   const problemDir = path.resolve(problemsDir, problem)
   if (fs.statSync(problemDir).isDirectory()) {
     const testcasesFile = path.resolve(problemDir, 'testcases')
+    const configFile = path.resolve(problemDir, 'config.js')
 
     const fn = require(problemDir)
+    const config = fs.existsSync(configFile) ? require(configFile) : {}
 
     describe(problem, () => {
-      test.each(buildTestcaseTable(testcasesFile))(
+      test.each(buildTestcaseTable(testcasesFile, config?.testcase?.input))(
         'Case #%#',
         (...args) => {
           expect(runTestcase(fn, args.slice(0, -1)))
