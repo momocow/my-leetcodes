097147206ed6f27de07f576526a419355837f73a
MomoCow <momocow.me@gmail.com>
2021-07-13 15:34:46 +0800

    :zap: (P125): Improve performance by early return

--- problems/125. Valid Palindrome/index.js
+++ problems/125. Valid Palindrome/index.js
@@ -11,13 +11,25 @@ function isAlphaNumeric (c) {
  */
 var isPalindrome = function (s) {
   for (let start = 0, end = s.length - 1; start <= end; start++, end--) {
+    let valid = true
     while (start < s.length && !isAlphaNumeric(s[start])) {
       start++
+      if (start > end) {
+        valid = false
+        break
+      }
+    }
+    if (!valid) {
+      break
     }
     while (end >= 0 && !isAlphaNumeric(s[end])) {
       end--
+      if (start > end) {
+        valid = false
+        break
+      }
     }
-    if (start > end) {
+    if (!valid) {
       break
     }
     if (s[start].toLowerCase() !== s[end].toLowerCase()) {
