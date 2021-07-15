440f2f16aad8441494c9c34c9e4f0ab45fd4e660
MomoCow <momocow.me@gmail.com>
2021-07-15 17:07:55 +0800

    :sparkles: (P611): Add P611.

---
+++ problems/611. Valid Triangle Number/index.js
@@ -0,0 +1,35 @@
+function binarySearch (arr, target, start = 0, end = arr.length) {
+  while (end - start > 1) {
+    if (arr[start] >= target) {
+      return start
+    }
+    if (arr[end - 1] < target) {
+      return end
+    }
+    const mid = Math.floor((start + end) / 2)
+    if (arr[mid] < target) {
+      start = mid
+    } else {
+      end = mid
+    }
+  }
+  return arr[start] >= target ? start : end
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangleNumber = function (nums) {
+  if (nums.length < 3) return 0
+  nums.sort((x, y) => x - y)
+  let count = 0
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      count += binarySearch(nums, nums[i] + nums[j], j + 1) - j - 1
+    }
+  }
+  return count
+}
+
+module.exports = triangleNumber
