fe9a7c6870653725a575798104b136ce2f135e87
MomoCow <momocow.me@gmail.com>
2021-07-15 17:11:37 +0800

    :zap: (P611): Improve by checking cases to ignore.

--- problems/611. Valid Triangle Number/index.js
+++ problems/611. Valid Triangle Number/index.js
@@ -25,7 +25,9 @@ var triangleNumber = function (nums) {
   nums.sort((x, y) => x - y)
   let count = 0
   for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] === 0) continue
     for (let j = i + 1; j < nums.length - 1; j++) {
+      if (nums[j] === 0) continue
       count += binarySearch(nums, nums[i] + nums[j], j + 1) - j - 1
     }
   }
