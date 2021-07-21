fd169840cc1a7c7774f53f5d6741736285e1d162
MomoCow <momocow.me@gmail.com>
2021-07-21 23:02:37 +0800

    :zap: (P838): Improve P838.

--- problems/838. Push Dominoes/index.js
+++ problems/838. Push Dominoes/index.js
@@ -3,30 +3,36 @@
  * @return {string}
  */
 var pushDominoes = function (dominoes) {
-  const arr = Array.from(dominoes)
-  let r = new Set()
-  let l = new Set()
-  do {
-    for (const i of r) {
-      arr[i] = 'R'
+  const arr = new Array(dominoes.length)
+  let t = false
+  let c = 0
+  for (let i = 0; i < dominoes.length; i++) {
+    if (t && dominoes[i] === '.') {
+      c /= 2
+    } else if (dominoes[i] === 'R') {
+      t = true
+      c = 1
+    } else {
+      t = false
+      c = 0
     }
-    for (const i of l) {
-      arr[i] = 'L'
+    arr[i] = c
+  }
+  t = false
+  c = 0
+  for (let j = dominoes.length - 1; j >= 0; j--) {
+    if (t && dominoes[j] === '.') {
+      c /= 2
+    } else if (dominoes[j] === 'L') {
+      t = true
+      c = 1
+    } else {
+      t = false
+      c = 0
     }
-    r = new Set()
-    l = new Set()
-
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === '.') {
-        if (arr[i - 1] === 'R' && arr[i + 1] !== 'L') {
-          r.add(i)
-        } else if (arr[i - 1] !== 'R' && arr[i + 1] === 'L') {
-          l.add(i)
-        }
-      }
-    }
-  } while (r.size + l.size > 0)
-  return arr.join('')
+    arr[j] -= c
+  }
+  return arr.map(n => n > 0 ? 'R' : n < 0 ? 'L' : '.').join('')
 }
 
 module.exports = pushDominoes
