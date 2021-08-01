f0afe10aa3da2ffc5d8b5ef91d6a9b9e37fec1a8
MomoCow <momocow.me@gmail.com>
2021-08-01 15:59:36 +0800

    :bug: (P1953): Fix misuse Math.max signature.

--- problems/1953. Maximum Number of Weeks for Which You Can Work/index.js
+++ problems/1953. Maximum Number of Weeks for Which You Can Work/index.js
@@ -4,7 +4,7 @@
 */
 var numberOfWeeks = function (milestones) {
   const sum = milestones.reduce((s, m) => s + m, 0)
-  const max = Math.max(milestones)
+  const max = Math.max(...milestones)
   const diff = 2 * max - sum
   if (diff > 1) {
     return 2 * sum - 2 * max + 1
